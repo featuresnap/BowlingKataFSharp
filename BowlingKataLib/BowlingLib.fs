490a782939cfa2d00ea4ea3194e3822cb1917629
Patrick Kelly <pkelly14159@hotmail.com>
2012-11-14 08:46:07 -0500

    Initial commit

---
+++ BowlingKataLib/BowlingLib.fs
@@ -0,0 +1,37 @@
+﻿module BowlingLib
+
+let addUp throws = Seq.sum throws
+
+let scoreThrows (throws: #seq<int>) = 
+    addUp throws
+
+let scoreCurrentFrame throws =
+    match throws with 
+    |a::b::tl when (a = 10)  ->
+        let c = List.head tl
+        ((a+b+c), b::tl)
+    |a::b::tl when (a + b = 10) ->
+        let c = List.head tl
+        ((a+b+c), tl)
+    |a::b::tl ->
+        ((a+b), tl)
+    |a::tl -> (a, [])
+    |[] -> (0, [])
+
+let rec scoreGameUtil currentScore throws frameNum= 
+    match (frameNum, throws) with 
+    |(_,[]) -> currentScore
+    |(10,_) -> currentScore
+    |_ -> 
+        let (currentFrame, remainingThrows) = scoreCurrentFrame throws
+        scoreGameUtil (currentScore + currentFrame) remainingThrows (frameNum + 1)
+        
+let scoreGame throws = scoreGameUtil 0 throws 0
+
+
+        
+        
+        
+        
+       
+
---
+++ BowlingKataTests/BowlingTests.fs
@@ -0,0 +1,77 @@
+﻿namespace BowlingTests
+
+open BowlingLib
+open NUnit.Framework
+
+[<TestFixture>]
+module public BowlingTests = 
+    
+    [<Test>]
+    let trueIsTrue() = 
+        Assert.AreEqual(true, true)
+
+    [<Test>]
+    let scoreNoThrowsIsZero() = 
+        let score = scoreThrows []
+        Assert.AreEqual(0,score)
+
+    [<Test>]
+    let scoreOneIsOne () = 
+        let score = scoreThrows [1]
+        Assert.AreEqual(1,score)
+
+    [<Test>]
+    let scoreTenIsTen () = 
+        let score = scoreThrows [1;9]
+        Assert.AreEqual(10,score)
+
+    [<Test>]
+    let scoreSpareInCurrentFrameIncludesTenPlusOneMoreThrow() = 
+        let (score, _) = scoreCurrentFrame [1;9;5]
+        Assert.AreEqual(15,score)
+
+    [<Test>]
+    let scoreStrikeInCurrentFrameIncludesTenPlusOneMoreThrow() = 
+        let (score, _) = scoreCurrentFrame [10;9;5]
+        Assert.AreEqual(24,score)
+    [<Test>]
+    let scoreThreeStrikesInARowGives30ForCurrentFrame() = 
+        let (score, _) = scoreCurrentFrame [10;10;10]
+        Assert.AreEqual(30,score)
+
+    [<Test>]
+    let scoreNonSparingFrameGivesRemainderOfInputLessFirstTwoBalls() =
+        let (score, remainingBalls) = scoreCurrentFrame [1;1;8]
+        Assert.AreEqual(8, List.head remainingBalls)
+
+    [<Test>]
+    let scoreSparingFrameGivesRemainderOfInputLessFirstTwoBalls() =
+        let (score, remainingBalls) = scoreCurrentFrame [1;9;8]
+        Assert.AreEqual(8, List.head remainingBalls)
+
+    [<Test>]
+    let scoreStrikingFrameGivesRemainderOfInputLessFirstBall() =
+        let (score, remainingBalls) = scoreCurrentFrame [10;9;8]
+        Assert.AreEqual(9, List.head remainingBalls)
+
+    [<Test>]
+    let perfectGameEqual300() =
+        let score = scoreGame [10;10;10;10;10;10;10;10;10;10;10;10] 
+        Assert.AreEqual(300, score)
+
+    [<Test>]
+    let allSparesOf9And1WithLastBallOf9Scores190() = 
+        let score =  
+            scoreGame 
+                [for i in 1..10 do 
+                    yield 9; yield 1; 
+                 yield 9]
+
+        Assert.AreEqual(190,score)
+
+
+
+
+
+
+
